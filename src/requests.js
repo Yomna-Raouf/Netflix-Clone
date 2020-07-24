12fc496d3bab280e572caaec036b852340e1502f
Yomna Raouf <54322994+Yomna-Raouf@users.noreply.github.com>
2020-07-24 10:46:04 -0700

    Update requests.js

--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = "19f84e11932abbc79e6d83f82d6d1045"; // use you API_KEY
+const API_KEY = "bbd316555d3af49dc52d62adb65fc7be";
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
@@ -7,7 +7,6 @@ const requests = {
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
     fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
     fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
 }
 
