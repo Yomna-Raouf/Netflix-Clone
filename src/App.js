05a887ea04b6b9b3be1fd714e71b80c0028417f2
yomna raouf <yomnaraouf99@gmail.com>
2020-07-24 19:39:11 -0700

    restore last version

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Row from  './Row';
 import Banner from './Banner';
 import Nav from './Nav';
@@ -7,22 +8,37 @@ import requests from './requests';
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Banner/>
-      <Row 
-        title="NETFLIX ORIGINALS" 
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
-      />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
-      <Row title="Documentries" fetchUrl={requests.fetchDocumentaries}/>
+
+    <BrowserRouter>
+      <Switch>
+        <Route path="/genres">
+        <div className="App">
+          <Nav />
+          <Banner/>
+          <Row 
+            title="NETFLIX ORIGINALS" 
+            fetchUrl={requests.fetchNetflixOriginals}
+            isLargeRow
+          />
+          <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+          <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+          <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+          <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+          <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+          <Row title="Documentries" fetchUrl={requests.fetchDocumentaries}/>
+      </div>  
+        </Route>
+      </Switch>
+    </BrowserRouter>
       
-    </div>
+       
+      
+    
+
+
+    /*
+    
+    */
   );
 }
 
