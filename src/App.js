bb519201e803ccaab9426637211a7c22fe183eee
yomna raouf <yomnaraouf99@gmail.com>
2020-07-24 19:05:21 -0700

    fixing a bug

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
-      <Row title="Horror Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
       <Row title="Documentries" fetchUrl={requests.fetchDocumentaries}/>
     </div>  
   
